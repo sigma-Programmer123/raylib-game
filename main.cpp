215dd6377449b2e58329d95f78e0f69bc4449bf1
sigma-Programmer123 <kardslegends@protonmail.com>
2026-09-25 20:15:26 +0700

    NO PLS

--- main.cpp
+++ main.cpp
@@ -8,15 +8,17 @@ void DrawUpdatedFrame() {
 }
 
 int main(void) {
-    Window window(800, 450, "I HATE THIS");
+    // Use procedural InitWindow instead of raylib-cpp stack objects
+    InitWindow(800, 450, "I HATE THIS");
     SetTargetFPS(60);
 
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(DrawUpdatedFrame, 60, 1);
 #else
-    while (!window.ShouldClose()) {
+    while (!WindowShouldClose()) {
         DrawUpdatedFrame();
     }
+    CloseWindow();
 #endif
 
     return 0;
