426d456a4128f58b6e16a6937e070462aa5495d2
sigma-Programmer123 <kardslegends@protonmail.com>
2026-09-25 19:34:26 +0700

    fuck

--- main.cpp
+++ main.cpp
@@ -3,20 +3,21 @@
 void DrawUpdatedFrame() {
     BeginDrawing();
         ClearBackground(DARKGRAY);
-        DrawText("Hello, world! (on mobile)", 100, 225, 32,
-                 RAYWHITE);
+        DrawText("Hello, world! (on mobile)", 100, 225, 32, RAYWHITE);
     EndDrawing();
 }
 
 int main(void) {
     Window window(800, 450, "I HATE THIS");
+    SetTargetFPS(60);
+
 #ifdef __EMSCRIPTEN__
     emscripten_set_main_loop(DrawUpdatedFrame, 60, 1);
 #else
-    SetTargetFPS(60);
     while (!window.ShouldClose()) {
         DrawUpdatedFrame();
     }
 #endif
+
     return 0;
 }
