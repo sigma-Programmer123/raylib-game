4264e7834c8158877ea87be3cc8b62d0f90daa0d
sigma-Programmer123 <kardslegends@protonmail.com>
2026-09-25 19:07:45 +0700

    updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,22 @@
 #include "user_core.hpp"
 
+void DrawUpdatedFrame() {
+    BeginDrawing();
+        ClearBackground(BLACK);
+        DrawText("Hello, world! (on mobile)", 640, 540, 64,
+                 RAYWHITE);
+    EndDrawing();
+}
+
 int main(void) {
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(DrawUpdatedFrame, 0, 1);
+#else
     SetTargetFPS(60);
+    Window window(1920, 1080, "I HATE THIS");
     while (!window.ShouldClose()) {
-        Window window(1920, 1080, "I HATE THIS");
-        BeginDrawing();
-            ClearBackground(BLACK);
-            DrawText("Hello, world! (on mobile)", 640, 540, 64,
-                     RAYWHITE);
-        EndDrawing();
+        DrawUpdatedFrame();
     }
+#endif
     return 0;
 }
